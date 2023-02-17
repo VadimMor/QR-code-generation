bc5eb510b6271f3707ee7f8953f9847325aa4c9a
Vadim Uniatiskiy <87434747+VadimMor@users.noreply.github.com>
2023-02-17 15:24:49 +0500

    Добавлено разделение информации на блоки

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let linkTest = "https://en.wikipedia.org/wiki/Enigma_machinesdfssdfsdfdf";
+let linkTest = "https://en.wikipedia.org/wiki/Enigma_machineuieyhgjkmhthtyjtneyubnhfdgrgrgfrtuiehg6urgfuymygbfuyvg";
 
 // Значения символов в буквенно-цифровом кодировании.
 const characterValuesInAlphanumericEncoding = {
@@ -46,6 +46,31 @@ const NumbersOfBlocksHight = [
     25, 34, 30, 32, 35, 37, 40, 42, 45, 48,
     51, 54, 57, 60, 63, 66, 70, 74, 77, 81
 ]
+
+// Количество байтов коррекции на один блок
+const numberOfCorrectionBytesPerBlock = [
+    17, 28, 22, 16, 22, 28, 26, 26, 24, 28,
+    24, 28, 22, 24, 24, 30, 28, 28, 26, 28,
+    30, 24, 30, 30, 30, 30, 30, 30, 30, 30,
+    30, 30, 30, 30, 30, 30, 30, 30, 30, 30
+]
+
+// Генирирующие многочлены
+const generatingPolynomials = {
+    7: [87, 229, 146, 149, 238, 102, 21],
+    10: [251, 67, 46, 61, 118, 70, 64, 94, 32, 45],
+    13: [74, 152, 176, 100, 86, 100, 106, 104, 130, 218, 206, 140, 78],
+    15: [	8, 183, 61, 91, 202, 37, 51, 58, 58, 237, 140, 124, 5, 99, 105],
+    16: [120, 104, 107, 109, 102, 161, 76, 3, 91, 191, 147, 169, 182, 194, 225, 120],
+    17: [43, 139, 206, 78, 43, 239, 123, 206, 214, 147, 24, 99, 150, 39, 243, 163, 136],
+    18: [215, 234, 158, 94, 184, 97, 118, 170, 79, 187, 152, 148, 252, 179, 5, 98, 96, 153],
+    20: [17, 60, 79, 50, 61, 163, 26, 187, 202, 180, 221, 225, 83, 239, 156, 164, 212, 212, 188, 190],
+    22: [210, 171, 247, 242, 93, 230, 14, 109, 221, 53, 200, 74, 8, 172, 98, 80, 219, 134, 160, 105, 165, 231],
+    24: [229, 121, 135, 48, 211, 117, 251, 126, 159, 180, 169, 152, 192, 226, 228, 218, 111, 0, 117, 232, 87, 96, 227, 21],
+    26: [173, 125, 158, 2, 103, 182, 118, 17, 145, 201, 111, 28, 165, 53, 161, 21, 245, 142, 13, 102, 48, 227, 153, 145, 218, 70],
+    28: [168, 223, 200, 104, 224, 234, 108, 180, 110, 190, 195, 147, 205, 27, 232, 201, 21, 43, 245, 87, 42, 195, 212, 119, 242, 37, 9, 123],
+    30: [41, 173, 145, 152, 216, 31, 179, 182, 50, 48, 110, 86, 239, 96, 222, 125, 42, 173, 226, 193, 224, 130, 156, 37, 251, 216, 238, 40, 192, 180]
+}
 
 
 // Кодирование данных. Буквенно-цифровое кодирование.
@@ -130,16 +155,16 @@ function AddingServiceFields(linkBitString, qrCodeVersion) {
     которое нужно для выбранной версии, то её надо дополнить чередующимися
     байтами 11101100 и 00010001
 */
-function filling(serviceFieldsBit, qrCodeVersion) {
+function filling(serviceFieldsBit, qrCodeVersion, repeatFill = 0) {
     if (serviceFieldsBit.length % 8 == 0) {
         if (serviceFieldsBit.length == tableMaximumAmountOfInformationHight[qrCodeVersion]) {
             return serviceFieldsBit
         } else {
-            if ((tableMaximumAmountOfInformationHight[qrCodeVersion] - serviceFieldsBit.length) % 16 == 0) {
-                return (serviceFieldsBit + '1110110000010001'.repeat((tableMaximumAmountOfInformationHight[qrCodeVersion] - serviceFieldsBit.length) / 16));
-            } {
-                return (serviceFieldsBit + '1110110000010001'.repeat(Math.ceil((tableMaximumAmountOfInformationHight[qrCodeVersion] - serviceFieldsBit.length) / 16)).slice(0, -8))
-            };
+            if (repeatFill % 2) {
+                return filling(serviceFieldsBit+"00010001", qrCodeVersion, ++repeatFill);
+            } else {
+                return filling(serviceFieldsBit+"11101100", qrCodeVersion, ++repeatFill);
+            }
         }
     } else {
         serviceFieldsBit =  serviceFieldsBit + '0'.repeat(Math.ceil(serviceFieldsBit.length / 8) * 8 - serviceFieldsBit.length);
@@ -154,18 +179,58 @@ function filling(serviceFieldsBit, qrCodeVersion) {
     уровня коррекции количество блоков.
 */
 function dividingInformationIntoBlocks(linkBitString, qrCodeVersion) {
-    if (NumbersOfBlocksHight[qrCodeVersion] == 1) {
-        return linkBitString
-    } else {
-        let NumberOfBlocksHight = NumbersOfBlocksHight[qrCodeVersion],
-        stringBit = [];
+    if (NumbersOfBlocksHight[qrCodeVersion] != 1) {
+        NumberOfBlocks = NumbersOfBlocksHight[qrCodeVersion],
+        linkBytesLenght = linkBitString.length / 8,
+        arrBytes = new Array();
 
-        if (linkBitString.length / 8 % NumberOfBlocksHight == 0) {
-            console.log(linkBitString.length / 8 % NumberOfBlocksHight)
+        if (linkBytesLenght % NumberOfBlocks) {
+            for (let i=0, plus=NumberOfBlocks - linkBytesLenght % NumberOfBlocks; i < NumberOfBlocks; i++) {
+                if (i < plus) {
+                    arrBytes.push(
+                        linkBitString.slice(
+                            i * Math.floor(linkBytesLenght / NumberOfBlocks) * 8,
+                            i * Math.floor(linkBytesLenght / NumberOfBlocks) * 8 + Math.floor(linkBytesLenght / NumberOfBlocks) * 8
+                        )
+                    )
+                } else if (i == plus) {
+                    arrBytes.push(
+                        linkBitString.slice(
+                            i * Math.floor(linkBytesLenght / NumberOfBlocks) * 8,
+                            i * Math.floor(linkBytesLenght / NumberOfBlocks) * 8 + Math.floor(linkBytesLenght / NumberOfBlocks) * 8 + 8
+                        )
+                    )
+                } else {
+                    arrBytes.push(
+                        linkBitString.slice(
+                            i * Math.floor(linkBytesLenght / NumberOfBlocks) * 8 + 8,
+                            i * Math.floor(linkBytesLenght / NumberOfBlocks) * 8 + Math.floor(linkBytesLenght / NumberOfBlocks) * 8 + 16
+                        )
+                    )
+                }
+            }
         } else {
-            console.log(linkBitString.length / 8 % NumberOfBlocksHight)
+            for (let i=0; i < NumberOfBlocks; i++) {
+                arrBytes.push(
+                    linkBitString.slice(
+                        i * linkBytesLenght / NumberOfBlocks * 8,
+                        i * linkBytesLenght / NumberOfBlocks * 8 + linkBytesLenght / NumberOfBlocks * 8
+                    )
+                )
+            }
         }
     }
+
+    return arrBytes
+}
+
+
+// Создание байтов коррекции
+/*
+
+*/
+function CreationOfCorrectionBytes() {
+
 }
 
 function main() {
@@ -175,7 +240,8 @@ function main() {
     linkBitString = serviceFields[0],
     qrCodeVersion = serviceFields[1];
     linkBitString = filling(linkBitString, qrCodeVersion);
-    dividingInformationIntoBlocks(linkBitString, qrCodeVersion)
+    let arrBytes = dividingInformationIntoBlocks(linkBitString, qrCodeVersion);
+    console.log(arrBytes[0].length / 8, qrCodeVersion)
 }
 
 main()
